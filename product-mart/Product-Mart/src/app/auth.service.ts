b6bac3057bec3bd066a3636f3a79feac3a0bfcb3
6Ankita11 <ank333ita@gmail.com>
2020-01-13 14:56:04 +0530

    adding registration page

--- product-mart/Product-Mart/src/app/auth.service.ts
+++ product-mart/Product-Mart/src/app/auth.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, Subject } from 'rxjs';
 
+export interface User{
+ email: string;
+ password: string;
+ fullname: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user$ = new Subject<User>();
   constructor() {}
 
    login(email: string, password: string) {
-     const loginCredentials = { email,password };
-     console.log('loginCredentials',loginCredentials);
+     const loginCredentials = { email, password };
+     console.log('login credentials', loginCredentials);
      return of (loginCredentials);
+  }
+  get user(){
+   return this.user$.asObservable();
+  }
+  register(user: any){
+    this.user$.next(user);
+   return of(user);
   }
 }
